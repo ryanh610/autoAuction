7aba2e9256c0f701b24b6b5170c2be54f70c4b5e
Matthew Carson <matthew.carson@pm.me>
2022-04-26 10:50:40 -0400

    Add handlebars

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const res = require('express/lib/response')
 const User = require('../models/User')
 
@@ -17,8 +18,35 @@ router.post('/signup', async (req, res) => {
     res.redirect('/')
   })
 })
+
 // TODO: Log in user stored in database
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({ where: { username: req.body.username }})
+
+  if (!user) return res.redirect('/login')
+
+  authenticated = await bcrypt.compare(req.body.password, user.password)
+
+  if (authenticated) {
+    req.session.save(() => {
+      req.session.user = user.get({ plain: true })
+  
+      res.redirect('/')
+    })
+  } else {
+    // Send flash message that 'username/password is incorrect
+    res.redirect('/login')
+  }
+
+})
 
 // TODO: Log out user, destroy session
+router.delete('/logout', (req, res) => {
+  console.log('hi')
+  if (!req.session.user) return res.redirect('/')
+  req.session.destroy(() => {
+    res.redirect('/') // Probably not necessary with our implementation
+  })
+})
 
 module.exports = router
