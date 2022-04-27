b6b3bba183d0c329d1ec7608dcd43732ebf08f61
ryanh610 <100244705+ryanh610@users.noreply.github.com>
2022-04-27 14:21:49 -0400

    fixed get all

--- controllers/api/carRoutes.js
+++ controllers/api/carRoutes.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
-const { Car, Bid } = require('../../models');
+const { Car, Bid, User } = require('../../models');
 
 router.get("/", async (req, res) => {
   try {
-    await Car.findAll({ include: {model: User, attributes: ['username']}, include: {model: Bid, attributes: ['price']}})
-    res.status(200).json(data)
+    let carData = await Car.findAll({ include: [{model: User, attributes: ['username']}, {model: Bid, attributes: ['price']}]})
+    res.status(200).json(carData)
   } catch(err) {res.status(400).json(err)}
 });
 
 router.get("/:id", async (req, res) => {
   try {  
-    Car.findOne({ where: {id: req.params.id}, include: {model: User, attributes: ['username']}, include: {model: Bid, attributes: ['price']}})
-    res.status(200).json(data)
+    let carData = Car.findOne({ where: {id: req.params.id}, include: [{model: User, attributes: ['username']}, {model: Bid, attributes: ['price']}]})
+    res.status(200).json(carData)
   } catch(err) {res.status(400).json(err)}
   });
   
