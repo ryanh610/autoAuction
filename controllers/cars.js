1159c3a53d7fa2f3d71b2b31cdd45e48f4d355eb
Matthew Carson <matthew.carson@pm.me>
2022-04-28 13:21:52 -0400

    Add countdown timer for car/auction

--- controllers/cars.js
+++ controllers/cars.js
@@ -18,11 +18,17 @@ router.get('/:id', async (req, res) => {
     ]
   })
   car.bids = car.bids.map(bid => bid.get({ plain: true }))
-
   if (!car) return res.status(404).render('errors/404')
+  car.get({ plain: true })
+  if (car.is_active()) {
+    var {days, hours, minutes, seconds} = car.time_values()
+  }
 
   res.render('cars/show', {
-    car: car.get({ plain: true }),
+    car: car.get({ plain: true}),
+    time_values: {
+      days, hours, minutes, seconds
+    },
     user: req.session.user
   })
 })
